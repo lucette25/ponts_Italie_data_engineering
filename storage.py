b2d8dd36ce4f2521893829f1e8c362c7e9394413
lucette25 <hermione.odjo@imsp-uac.org>
2022-05-03 09:53:24 +0200

    deploiement_heroku

--- storage.py
+++ storage.py
@@ -2,7 +2,5 @@ import getData
 import sqlalchemy
 
 data=getData.ponts_df
-engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres:Lucette@localhost:5432/ponts')
-data.to_sql('ponts_table', engine)
-
+engine = sqlalchemy.create_engine('postgresql+psycopg2://ywczmgutsqcrov:8f83bb47c1a0d3f90db7e004786505cdde184c1ffc95ac855bdc7a4b11d531a7@ec2-3-217-113-25.compute-1.amazonaws.com:5432/d52673l1t0ik3a')
 print(data)
