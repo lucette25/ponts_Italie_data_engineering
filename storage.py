fd13796d7b9786393ea893431ff3c7e3a965751f
lucette25 <hermione.odjo@imsp-uac.org>
2022-05-05 11:48:59 +0200

    deploiement_heroku

--- storage.py
+++ storage.py
@@ -5,7 +5,7 @@ import sqlalchemy
 def toDataBase():
     data = getData()
     engine = sqlalchemy.create_engine(
-        'postgresql+psycopg2://ywczmgutsqcrov:8f83bb47c1a0d3f90db7e004786505cdde184c1ffc95ac855bdc7a4b11d531a7@ec2-3-217-113-25.compute-1.amazonaws.com:5432/d52673l1t0ik3a')
+        'postgresql+psycopg2://grlfqghirgpfqm:243ca0d2a4c5dfdab173f97889b41078abfb998568b96f57f427fea8f04b2486@ec2-52-5-110-35.compute-1.amazonaws.com:5432/d95i7uhi4ll0fb')
     data.to_sql('ponts_table', engine, if_exists='replace')
 
 
